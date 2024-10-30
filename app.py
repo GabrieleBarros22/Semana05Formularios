b626d615a652c5ba1655220209ee741dd31312a7
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-30 17:17:03 -0300

    att

--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def index():
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    
+
     return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+
+if __name__ == '__main__':
+    app.run(debug=True)
