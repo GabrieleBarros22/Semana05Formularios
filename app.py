8c63dfb074948364fdf8652b00376a1ac310be82
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-31 14:19:41 -0300

    att

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from datetime import datetime
-from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Chave forte'
-Bootstrap(app)
 
+# Formulário
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
+# Rota principal
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -30,7 +21,7 @@ def index():
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run(debug=True)
