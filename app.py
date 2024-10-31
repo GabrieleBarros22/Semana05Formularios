5e7465fe5f69afa7cbc5dedb6d58d7ea3fe7883f
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-31 15:27:55 -0300

    att

--- app.py
+++ app.py
@@ -15,12 +15,11 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name="Test Name")  # Teste com um nome fixo
-
+  form = NameForm()
+  if form.validate_on_submit():
+    session['name'] = form.name.data
+    return redirect(url_for('index'))
+  return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
