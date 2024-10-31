3f716f613280c19857aa41bf1a3bf83877019370
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-31 15:40:13 -0300

    att

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, session, redirect, url_for
 from datetime import datetime
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -8,6 +9,7 @@ from wtforms.validators import DataRequired
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abc123'
 Bootstrap(app)
+Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
