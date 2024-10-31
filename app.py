14ff9bcc34c8a40549d5b46ff9bec95d8bb053ab
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-31 14:53:36 -0300

    att

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for
 from datetime import datetime
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -24,14 +24,11 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+  form = NameForm()
+  if form.validate_on_submit():
+    session['name'] = form.name.data
+    return redirect(url_for('index'))
+  return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run(debug=True)
