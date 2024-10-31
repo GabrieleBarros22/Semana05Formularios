1d9e532d2856f9a9f616fee25a8c4ea8d85f1b70
GabrieleBarros22 <gabrielebarros22@hotmail.com>
2024-10-31 14:26:32 -0300

    att

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
+from datetime import datetime
+from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import secrets  # Importa a biblioteca secrets
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Chave forte'
+app.config['SECRET_KEY'] = secrets.token_hex(16)  # Gera uma chave secreta aleatória
+Bootstrap(app)
 
-# Formulário
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Rota principal
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
@@ -21,7 +31,7 @@ def index():
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True)
